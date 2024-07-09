6a0f99486cea567eddadf6d4cc904ed6f5536761
lis0517 <01034895017a@gmail.com>
2024-07-09 15:53:46 +0900

    Create project

---
+++ src/main/java/com/sparta/bitbucket/BitbucketApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.bitbucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitbucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitbucketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/bitbucket/BitbucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.bitbucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitbucketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

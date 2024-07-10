901c31003d24ec2e2f203b6deab8acd66f828a6d
lis0517 <01034895017a@gmail.com>
2024-07-10 20:44:01 +0900

    feat: Create Timestamped
    Timestamped 추가
    
    ADD
        - Timestamped를 추가하였습니다.
    UPDATE
        - User가 Timestamped를 상속받도록 변경했습니다.

---
+++ src/main/java/com/sparta/bitbucket/common/entity/Timestamped.java
@@ -0,0 +1,29 @@
+package com.sparta.bitbucket.common.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Timestamped {
+
+	@CreatedDate
+	@Column(updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime updatedAt;
+}

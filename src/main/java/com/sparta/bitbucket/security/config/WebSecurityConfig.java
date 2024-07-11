0d49f9cc5374d00da4cdd7f93ca05ff7ed34c36a
lis0517 <01034895017a@gmail.com>
2024-07-11 17:25:10 +0900

    feat: Update authorize
    접근 권한 관련 수정
    페이지에 접근할 수 있도록 /** 추가
    POST로 된 모든 auth를 허가했는데 login만 허가하도록 변경

--- src/main/java/com/sparta/bitbucket/security/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/bitbucket/security/config/WebSecurityConfig.java
@@ -76,9 +76,9 @@ public class WebSecurityConfig {
 
 		http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers(
 				PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
-			.requestMatchers("/").permitAll() // 메인 페이지 요청 허가
+			.requestMatchers("/**").permitAll() // 메인 페이지 요청 허가
 			.requestMatchers("/api/users").permitAll() // 회원가입 요청 허가
-			.requestMatchers(HttpMethod.POST ,"/api/auth/**").permitAll() // 로그인 요청 허가
+			.requestMatchers(HttpMethod.POST ,"/api/auth/login").permitAll() // 로그인 요청 허가
 			.anyRequest().authenticated() // 그 외 모든 요청 인증처리
 		);
 

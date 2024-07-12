df8374c72a5d9a22fd42f466151883bc26348334
seungsuuu <tmdtn013@naver.com>
2024-07-12 13:06:02 +0900

    feat: Exception Handler
    Exception Handler 기본적인 Exception 만 추가해서 구현
    
    ADD
        - GlobalExceptionHandler.java
    
    UPDATE
        - validation Exception handler
        - DB Exception handler
        - IllegalArgumentException handler
    
    BREAKING CHANGE(중요한 변경 사항): 일단 IllegalArgumentException 을 추가하긴 했는데, 각자 커스텀 Exception 을 추가하면 수정하겠습니다.

---
+++ src/main/java/com/sparta/bitbucket/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.sparta.bitbucket.exception.handler;
+
+import java.util.stream.Collectors;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.sparta.bitbucket.common.dto.MessageResponseDto;
+import com.sparta.bitbucket.common.util.ResponseFactory;
+
+import jakarta.validation.ConstraintViolationException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	// validation exception handler : valid 에러 메세지 클라이언트에 전달
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<MessageResponseDto> MethodArgumentNotValidExceptionHandler(
+		MethodArgumentNotValidException e) {
+
+		String errorMessages = e.getBindingResult().getAllErrors().stream()
+			.map(ObjectError::getDefaultMessage)
+			.collect(Collectors.joining(", "));
+
+		return ResponseFactory.badRequest(errorMessages);
+	}
+
+	// DB exception handler : DB 저장 에러 메세지 클라이언트에 전달
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<MessageResponseDto> ConstraintViolationExceptionHandler(ConstraintViolationException e) {
+
+		StringBuilder errorMessages = new StringBuilder();
+
+		e.getConstraintViolations().forEach(violation -> {
+			errorMessages.append(violation.getPropertyPath() + ": " + violation.getMessage() + "\n");
+		});
+
+		return ResponseFactory.badRequest(errorMessages.toString());
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<MessageResponseDto> IllegalArgumentExceptionHandler(IllegalArgumentException e) {
+
+		String errorMessage = "Exception caught: " + e.getMessage();
+
+		return ResponseFactory.badRequest(errorMessage);
+	}
+
+}

097f1505ec53817c5c3903caff81627169b68ee6
KIM <wltn9169@naver.com>
2024-07-14 21:31:52 +0900

    ♻️refactor
    ColumnController.java
    - url 변경
    - delete return ResponseFactory.noContent 변경
    ColumnService.java
    - Exception -> CustomException 변경
    - url 변경한 boardId 변수 추가
    
    ErrorCode.java
    - column에 사용할 에러코드들 추가
    GlobalExceptionHandler.java
    - customException 발생 시  사용차 body 화면에 출력하기 위해 추가
    -

--- src/main/java/com/sparta/bitbucket/column/dto/CreateColumnRequestDto.java
+++ src/main/java/com/sparta/bitbucket/column/dto/CreateColumnRequestDto.java
@@ -1,7 +1,6 @@
 package com.sparta.bitbucket.column.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
@@ -9,9 +8,6 @@ public class CreateColumnRequestDto {
 
 	@NotBlank
 	private String title;
-
-	@NotNull
-	private Long boardId;
 
 	private int orders;
 }
--- src/main/java/com/sparta/bitbucket/column/dto/EditColumnRequestDto.java
+++ src/main/java/com/sparta/bitbucket/column/dto/EditColumnRequestDto.java
@@ -1,13 +1,10 @@
 package com.sparta.bitbucket.column.dto;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class EditColumnRequestDto {
 
 	private String title;
-	@NotNull
-	private Long boardId;
 	private Integer orders;
 }
